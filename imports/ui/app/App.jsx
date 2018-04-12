5f05dbf91b8caf2790b3a7196ed29d54bd468519
Esteban Ortiz <ematiasortiz@gmail.com>
2018-04-11 23:59:21 -0300

    refator improved UX

--- imports/ui/app/App.jsx
+++ imports/ui/app/App.jsx
@@ -8,32 +8,21 @@ export default class App extends Component {
 	constructor(props) {
     super(props);
     this.state = {
-      formIsVisible: true,
+      
       playersFromState: [{ name: "Diego" }, { name: "Matias" }, { name: "Pele"}, , { name: "Maradona"}]
     };
   }
 
-  toggleForm = () => {
-    this.setState({ formIsVisible: !this.state.formIsVisible });
-    console.log('click en togable');
-  };
-
   addPlayer = player => {
-    let { playersFromState } = this.state;
+    let { playersFromState, formIsVisible } = this.state;
     this.setState({ playersFromState: playersFromState.concat(player) });
     console.log('click add');
   };
 
   render() {
   	let { playersFromState, formIsVisible } = this.state;
-    return (
-    	<div>
-        <PlayersList playersAsProps={playersFromState}/>
-        <TogglebleForm visible={formIsVisible} handleCreate={this.addPlayer}/>
-        <a className="waves-effect waves-light btn" onClick={this.toggleForm}>
-          Add Player
-        </a>
-      </div>
+    return ( 	
+        <PlayersList playersAsProps={playersFromState} submitPlayer={this.addPlayer}/>
     );
   }
 }
--- imports/ui/components/lists/players/PlayersList.jsx
+++ imports/ui/components/lists/players/PlayersList.jsx
@@ -2,31 +2,56 @@ import React, { Component } from "react";
 import { PropTypes } from "prop-types";
 
 import PlayerListItem from "./PlayerListItem";
+import ToggableForm from "../../forms/players/ToggableForm";
 
 class PlayersList extends Component {
 
 	constructor(props) {
 	  super(props);
 	  this.state = {
-	    loading: true
+	    visible: props.playersAsProps.length > 0 ? false : true,
+      showButton: true
 	  };
 	}
 
-  render() {
-  	const { playersAsProps } = this.props;
+  toggleForm = () => {
+    this.setState({
+      visible: !this.state.visible,
+      showButton: !this.state.showButton
+    });
+  };
 
+  handleSubmit = player => {
+    this.props.submitPlayer(player);
+    this.setState({
+      visible: !this.state.visible,
+      showButton: !this.state.showButton
+    });
+  };
+  render() {
+  	const { handleSubmit, playersAsProps } = this.props;
+    const { visible, showButton } = this.state;
     return (
-    	<ul className="collection with-header">
-        <li className="collection-header">
-          <h4>Lista de Jugadores</h4>
-        </li>
-        {playersAsProps.map(player => <PlayerListItem player={player} />)}
-      </ul>
+    	<div>
+        <ul className="collection with-header">
+          <li className="collection-header">
+            <h4>Lista de Amigos</h4>
+          </li>
+          {playersAsProps.map(player => <PlayerListItem player={player} />)}
+        </ul>
+        <ToggableForm visible={visible} handleCreate={this.handleSubmit} />
+          {showButton && (
+            <a className="btn" onClick={this.toggleForm}>
+              Nuevo Amigo
+            </a>
+          )}
+      </div>
     );
   }
 }
 
 PlayersList.propTypes = {
+  submitPlayer: PropTypes.func.isRequired,
   playersAsProps: PropTypes.array.isRequired
 };
 
